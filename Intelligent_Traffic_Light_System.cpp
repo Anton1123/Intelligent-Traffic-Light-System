3485a4a348672ed66f2e310e1fe42d59b6fe5b6a
Anton Savelyev <antonsavelyev@mail.fresnostate.edu>
2015-12-09 13:09:45 -0800

    final version

--- Intelligent_Traffic_Light_System.cpp
+++ Intelligent_Traffic_Light_System.cpp
@@ -1,5 +1,5 @@
 // Anton Savelyev
-// Project: Intelligent Traffic Light System
+// Project: Intelligent Traffic Light System (Final)
 // Semester: Fall '15 @ Fresno State
 // Class: CSCI 144
 // Professor: Dr. Ming Li
@@ -16,18 +16,29 @@
 #include <unistd.h> // for usleep()
 #include <ctime> //for clock(), clock_t, CLOCKS_PER_SEC
 
-#define CARS 10 // # cars coming from each direction. 40 cars total
-#define RUN_TIME 125 // 125 seconds (5 seconds longer than it should take to run)
+#define CARS 100 // # cars all together
+#define RUN_TIME 200 // seconds the program will wait for to allow all cars to pass
 
 using namespace std;
 
 int globalID; // global ID for each car arriving at the intersection
 
-queue<int> globalQ; //a global Q
+// a queue for each direction.
+queue<int> northQ;
+queue<int> eastQ;
+queue<int> southQ;
+queue<int> westQ;
+queue<int> intersectionQ;
 
 pthread_t threadID;
-// a lock for each queue/direction and for the intersection (globalQ).
-pthread_mutex_t globalQLock;
+
+// a lock for each queue/direction.
+pthread_mutex_t northLock;
+pthread_mutex_t eastLock;
+pthread_mutex_t southLock;
+pthread_mutex_t westLock;
+pthread_mutex_t intersectionQLock;
+
 pthread_mutex_t globalIDLock; // lock for changing the globalid (i.e. car id)
 pthread_mutex_t intersectionLock; // lock for one car passing through the intersection
 
@@ -39,182 +50,217 @@ int getrand(int min,int max) //return a number x s.t. min <= x < max
 void init()
 {
     globalID = 1; //first car will have ID = 1
+    pthread_mutex_init(&northLock, NULL);
+    pthread_mutex_init(&eastLock, NULL);
+    pthread_mutex_init(&southLock, NULL);
+    pthread_mutex_init(&westLock, NULL);
+    pthread_mutex_init(&intersectionQLock, NULL);
     pthread_mutex_init(&globalIDLock, NULL);
     pthread_mutex_init(&intersectionLock, NULL);
-    pthread_mutex_init(&globalQLock, NULL);
 }
-// Now will test to create an intersection with only 1 direction. North
+
+// This is a function for a car moving north.
 void *north(void *null)
 {
+
     int localID;
     double duration; //for checking how long a car will be waiting at the front of its lane
-    clock_t start; //variable will be used to calculate wait time
+    clock_t start;
 
+    pthread_mutex_lock(&northLock); // locking the queue
     pthread_mutex_lock(&globalIDLock); // locking globalIDLock mutex in order to update globalID
     localID = globalID++; // update globalID after reserving that ID for a car in north lane
-    pthread_mutex_unlock(&globalIDLock); // releasing globalID
+    pthread_mutex_unlock(&globalIDLock);
+    northQ.push(localID); // pushing the local car into northQ.
+    pthread_mutex_unlock(&northLock);
 
-    usleep(getrand(100000, 1000000)); //lets say it takes somewhere between 1/10th of a second and 1 seconds to get to the intersection.
-    pthread_mutex_lock(&globalQLock); // locking the queue to push car
-    globalQ.push(localID);
-    pthread_mutex_unlock(&globalQLock);
+    sleep(2); //say it takes 2 seconds to get to the intersection
+    pthread_mutex_lock(&intersectionQLock);
+    intersectionQ.push(localID);
+    pthread_mutex_unlock(&intersectionQLock);
     start = clock(); // Now the car has arrived at intersection. Let's start the timer.
-
 
     while(1) //Checking cars properties here
     {
-        if (localID == globalQ.front()) // Current Car is in the front of its lane and first at the intersection. Lets Proceed
+        if ((localID == northQ.front()) && (localID == intersectionQ.front())) // Current Car is in the front of the lane... Lets Proceed
         {
             break;
         }
-        else //Current car is not first in line. Lets wait
+        else //Current car is not in front on its lane. Lets wait
         {
-            usleep(10); // sleep for 10 microsecond to allow for other cars to proceed if they must
+            usleep(1); // sleep for 1 microsecond to allow for other cars to proceed if they must
             continue;
         }
     }
     // Car is in the front so let's proceed to allow it to pass through intersection.
     pthread_mutex_lock(&intersectionLock); // need to lock the intersection. Function call will block until mutex is available
-    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC; //car will go through the intersecion. this is how long it has been waiting.
-    globalQ.pop(); // taking it off the line
+    duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
+    northQ.pop();
+    intersectionQ.pop();
     cout << "Car from NORTH lane with ID: " << localID << " ENTERING the intersection." << endl;
     cout << "It has been waiting at the light for: "<< duration<<" seconds."<<endl;
-    sleep(3); // takes 3 seconds to drive through the intersection
+    sleep(3);
     cout << "Car from NORTH lane with ID: " << localID << " LEAVING the intersection." << endl<<endl;
-    pthread_mutex_unlock(&intersectionLock); //releasing the intersection
+    pthread_mutex_unlock(&intersectionLock);
 }
 
 void *east(void *null)
 {
-    int localID;
-    double duration; //for checking how long a car will be waiting at the front of its lane
-    clock_t start; //variable will be used to calculate wait time
 
+    int localID;
+    double duration;
+    clock_t start;
+
+    pthread_mutex_lock(&eastLock); // locking the queue
     pthread_mutex_lock(&globalIDLock); // locking globalIDLock mutex in order to update globalID
     localID = globalID++; // update globalID after reserving that ID for a car in north lane
-    pthread_mutex_unlock(&globalIDLock); // releasing globalID
+    pthread_mutex_unlock(&globalIDLock);
+    eastQ.push(localID); // pushing the local car into northQ.
+    pthread_mutex_unlock(&eastLock);
 
-    usleep(getrand(100000, 1000000)); //lets say it takes somewhere between 1/10th of a second and 1 seconds to get to the intersection.
-    pthread_mutex_lock(&globalQLock); // locking the queue to push car
-    globalQ.push(localID);
-    pthread_mutex_unlock(&globalQLock);
-    start = clock(); // Now the car has arrived at intersection. Let's start the timer.
+    sleep(2); //say it takes 2 seconds to get to the intersection
+    pthread_mutex_lock(&intersectionQLock);
+    intersectionQ.push(localID);
+    pthread_mutex_unlock(&intersectionQLock);
+    start = clock();
 
     while(1) //Checking cars properties here
     {
-        if (localID == globalQ.front()) // Current Car is in the front of its lane and first at the intersection. Lets Proceed
+        if ((localID == eastQ.front()) && (localID == intersectionQ.front())) // Current Car is in the front of the lane... Lets Proceed
         {
             break;
         }
-        else //Current car is not first in line. Lets wait
+        else //Current car is not in front on its lane. Lets wait
         {
-            usleep(10); // sleep for 10 microsecond to allow for other cars to proceed if they must
+            usleep(1); // sleep for 1 microsecond to allow for other cars to proceed if they must
             continue;
         }
     }
     // Car is in the front so let's proceed to allow it to pass through intersection.
     pthread_mutex_lock(&intersectionLock); // need to lock the intersection. Function call will block until mutex is available
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    globalQ.pop();
+    eastQ.pop();
+    intersectionQ.pop();
     cout << "Car from EAST lane with ID:  " << localID << " ENTERING the intersection." << endl;
     cout << "It has been waiting at the light for: "<<duration<<" seconds."<<endl;
     sleep(3);
     cout << "Car from EAST lane with ID:  " << localID << " LEAVING the intersection." << endl <<endl;
-    pthread_mutex_unlock(&intersectionLock); // give other cars a chance to pass
+    pthread_mutex_unlock(&intersectionLock);
 }
-
 
 void *south(void *null)
 {
+
     int localID;
-    double duration; //for checking how long a car will be waiting at the front of its lane
-    clock_t start; //variable will be used to calculate wait time
+    double duration;
+    clock_t start;
 
-    pthread_mutex_lock(&globalIDLock); // locking globalIDLock mutex in order to update globalID
+    pthread_mutex_lock (&southLock); // locking the queue
+    pthread_mutex_lock (&globalIDLock); // locking globalIDLock mutex in order to update globalID
     localID = globalID++; // update globalID after reserving that ID for a car in north lane
-    pthread_mutex_unlock(&globalIDLock); // releasing globalID
+    pthread_mutex_unlock (&globalIDLock);
+    southQ.push(localID); // pushing the local car into northQ.
+    pthread_mutex_unlock (&southLock);
 
-    usleep(getrand(100000, 1000000)); //lets say it takes somewhere between 1/10th of a second and 1 seconds to get to the intersection.
-    pthread_mutex_lock(&globalQLock); // locking the queue to push car
-    globalQ.push(localID);
-    pthread_mutex_unlock(&globalQLock);
-    start = clock(); // Now the car has arrived at intersection. Let's start the timer.
+    sleep(2); //say it takes 2 seconds to get to the intersection
+    pthread_mutex_lock(&intersectionQLock);
+    intersectionQ.push(localID);
+    pthread_mutex_unlock(&intersectionQLock);
+    start = clock();
 
     while(1) //Checking cars properties here
     {
-        if (localID == globalQ.front()) // Current Car is in the front of its lane and first at the intersection. Lets Proceed
+        if ((localID == southQ.front()) && (localID == intersectionQ.front())) // Current Car is in the front of the lane... Lets Proceed
         {
             break;
         }
-        else //Current car is not first in line. Lets wait
+        else //Current car is not in front on its lane. Lets wait
         {
-            usleep(10); // sleep for 10 microsecond to allow for other cars to proceed if they must
+            usleep(1); // sleep for 1 microsecond to allow for other cars to proceed if they must
             continue;
         }
     }
     // Car is in the front so let's proceed to allow it to pass through intersection.
     pthread_mutex_lock(&intersectionLock); // need to lock the intersection. Function call will block until mutex is available
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    globalQ.pop();
+    southQ.pop();
+    intersectionQ.pop();
     cout << "Car from SOUTH lane with ID: " << localID << " ENTERING the intersection." << endl;
     cout << "It has been waiting at the light for: "<<duration<< " seconds."<<endl;
     sleep(3);
     cout << "Car from SOUTH lane with ID: " << localID << " LEAVING the intersection." << endl<<endl;
-    pthread_mutex_unlock(&intersectionLock); // give other cars a chance to pass
+    pthread_mutex_unlock(&intersectionLock);
 }
-
 
 void *west(void *null)
 {
+
     int localID;
-    double duration; //for checking how long a car will be waiting at the front of its lane
-    clock_t start; //variable will be used to calculate wait time
+    double duration;
+    clock_t start;
 
-    pthread_mutex_lock(&globalIDLock); // locking globalIDLock mutex in order to update globalID
+    pthread_mutex_lock (&westLock); // locking the queue
+    pthread_mutex_lock (&globalIDLock); // locking globalIDLock mutex in order to update globalID
     localID = globalID++; // update globalID after reserving that ID for a car in north lane
-    pthread_mutex_unlock(&globalIDLock); // releasing globalID
+    pthread_mutex_unlock (&globalIDLock);
+    westQ.push(localID); // pushing the local car into northQ.
+    pthread_mutex_unlock (&westLock);
 
-    usleep(getrand(100000, 1000000)); //lets say it takes somewhere between 1/10th of a second and 1 seconds to get to the intersection.
-
-    pthread_mutex_lock(&globalQLock); // locking the queue to push car
-    globalQ.push(localID);
-    pthread_mutex_unlock(&globalQLock);
-    start = clock(); // Now the car has arrived at intersection. Let's start the timer.
+    sleep(2); //say it takes 2 seconds to get to the intersection
+    pthread_mutex_lock(&intersectionQLock);
+    intersectionQ.push(localID);
+    pthread_mutex_unlock(&intersectionQLock);
+    start = clock();
 
     while(1) //Checking cars properties here
     {
-        if (localID == globalQ.front()) // Current Car is in the front of its lane and first at the intersection. Lets Proceed
+        if ((localID == westQ.front()) && (localID == intersectionQ.front())) // Current Car is in the front of the lane... Lets Proceed
         {
             break;
         }
-        else //Current car is not first in line. Lets wait
+        else //Current car is not in front on its lane. Lets wait
         {
-            usleep(10); // sleep for 10 microsecond to allow for other cars to proceed if they must
+            usleep(1); // sleep for 1 microsecond to allow for other cars to proceed if they must
             continue;
         }
     }
     // Car is in the front so let's proceed to allow it to pass through intersection.
     pthread_mutex_lock(&intersectionLock); // need to lock the intersection. Function call will block until mutex is available
     duration = ( std::clock() - start ) / (double) CLOCKS_PER_SEC;
-    globalQ.pop();
+    westQ.pop();
+    intersectionQ.pop();
     cout << "Car from WEST lane with ID:  " << localID << " ENTERING the intersection." << endl;
     cout << "It has been waiting for: "<< duration <<" seconds."<< endl;
     sleep(3);
     cout << "Car from WEST lane with ID:  " << localID << " LEAVING the intersection." << endl<<endl;
-    pthread_mutex_unlock(&intersectionLock); // give other cars a chance to pass
+    pthread_mutex_unlock(&intersectionLock);
 }
 
 int main()
 {
     init();
     srand(time(0));
+    int k = 0;
     for(int i = 0; i < CARS; i++) //first car will be car with ID 1; Last ID is 40
     {
-        pthread_create (&threadID, NULL, north, NULL);
-        pthread_create (&threadID, NULL, east, NULL);
-        pthread_create (&threadID, NULL, south, NULL);
-        pthread_create (&threadID, NULL, west, NULL);
-        usleep(10);
+        k = getrand(1,5);
+        if(k == 1)
+        {
+            pthread_create (&threadID, NULL, north, NULL);
+        }
+        else if (k == 2)
+        {
+            pthread_create (&threadID, NULL, east, NULL);
+        }
+        else if (k == 3)
+        {
+            pthread_create (&threadID, NULL, south, NULL);
+        }
+        else
+        {
+            pthread_create (&threadID, NULL, west, NULL);
+        }
+        sleep(1); //a car will be created every second.
     }
     sleep(RUN_TIME); //sleep for sufficient times to allow for all threads to finish running.
     cout << "Finished." << endl;
